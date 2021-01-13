6047fc3dac1f90467650dce5e496d0993226c1c8
Yang Gu <yg2349@gmail.com>
2021-01-13 16:10:41 -0500

    getMaxSum and Anagrams

---
+++ Anagrams/anagram.js
@@ -0,0 +1,25 @@
+const charCount = string => {
+    const table = {};
+  
+    for (let char of string.replace(/\W/g, "").toLowerCase())
+      table[char] = table[char] + 1 || 1;
+  
+    return table;
+  };
+  
+  const anagrams = (stringA, stringB) => {
+    const charCountA = charCount(stringA);
+    const charCountB = charCount(stringB);
+  
+    if (Object.keys(charCountA).length !== Object.keys(charCountB).length)
+      return false;
+  
+    for (let char in charCountA)
+      if (charCountA[char] !== charCountB[char]) return false;
+  
+    return true;
+  };
+  
+  const _sort = string => string.replace(/\W/g, "").toLowerCase().split("").sort().join("");
+  const _anagrams = (stringA, stringB) => _sort(stringA) === _sort(stringB);
+  
--- Anagrams/readme.md
+++ Anagrams/readme.md
@@ -1,4 +1,4 @@
-6. Anagrams
+Anagrams
 Anagrams are words or phrases that contain the same number of characters. Create a function that checks for this.
 describe("Anagrams", () => {
  it("Should implement anagrams", () => {
