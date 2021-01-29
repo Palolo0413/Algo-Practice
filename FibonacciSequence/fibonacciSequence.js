dba1450bcebc2631f7d35d8483b7bd445e4214d7
Yang Gu <yg2349@gmail.com>
2021-01-29 12:43:23 -0500

    fibonacci

--- FibonacciSequence/fibonacciSequence.js
+++ FibonacciSequence/fibonacciSequence.js
@@ -1,12 +1,21 @@
-// solution 1
+// solution 1 RECURSION
 function fibonacci(num){
 if (num <= 1) return 1
 return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-// solution 2
+//solution 2 LOOOOP
+function fibonacci(n) {
+	const list = [0, 1];
+	for (let x = 2; x < n + 1; x += 1) {
+		list.push(list[x - 2] + list[x - 1]);
+	}
+	return list[n];
+}
+
+// solution 3
 function fibonacci(num){
-    var a = 1, b = 0, temp;
+    let a = 1, b = 0, temp;
   
     while (num >= 0){
       temp = a;
@@ -18,7 +27,7 @@ function fibonacci(num){
     return b;
   }
 
-// solution 3
+// solution 4
 function fibonacci(num, memo) {
     memo = memo || {};
   
